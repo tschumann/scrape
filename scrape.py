1f66ba7464fcf32c969a164b3e9fff332803ae4d
tschumann <schumann.tom@gmail.com>
2015-09-04 21:22:46 -0700

    Improving file type handling.

--- scrape.py
+++ scrape.py
@@ -21,36 +21,47 @@ def process_page(url):
         print("Skipping external link to", url)
         return
 
+    # if we've already visited this page
     if normalised_url in visited_urls:
         return
     else:
+        # remember that we visited the page
         visited_urls.append(visited_urls)
 
+    # download the page itself
     response = requests.get(url)
 
-    # TODO: handle redirects and things that aren't errors
-    if response.status_code != 200:
+    if not response.ok:
         print("Could not access", url)
         return
 
-    # TODO: check mimetype here? parse HTML, download otherwise?
+    if 'content-type' in response.headers:
+        content_type = response.headers['content-type']
 
-    # parse the response HTML
-    soup = bs4.BeautifulSoup(response.text)
+        if content_type == 'text/html':
+            # parse the response HTML
+            soup = bs4.BeautifulSoup(response.text)
 
-    # find all links and media
-    # TODO: handle embed and object
-    links = soup.find_all("a")
-    sounds = soup.find_all("audio")
-    images = soup.find_all("img")
-    scripts = soup.find_all("script")
-    videos = soup.find_all("video")
+            # find all links and media
+            # TODO: handle embed and object
+            links = soup.find_all("a")
+            sounds = soup.find_all("audio")
+            images = soup.find_all("img")
+            scripts = soup.find_all("script")
+            videos = soup.find_all("video")
 
-    page = open(split_url.path or 'index.html')
-    page.write(response.text)
+            page = open(split_url.path or 'index.html')
+            page.write(response.text)
 
-    for link in links:
-        process_page(link.href)
+            # process all child pages
+            for link in links:
+                process_page(link.href)
+        else:
+            asset = open(split_url.path)
+            asset.write(response.text)
+    else:
+        # TODO: possible to not have a mime-type? check if it looks like HTML?
+        pass
 
 if len(sys.argv) < 2:
     print("No site specified")
